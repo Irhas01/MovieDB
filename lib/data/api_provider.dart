fa72ffd3b406569d114f1fe7b3ec32413cf28362
Irhas007 <e41211600@student.polije.ac.id>
2022-11-15 22:16:35 +0700

    Update

--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -1,103 +1,22 @@
-class PopularMovies {
-  late int page;
-  late int totalResults;
-  late int totalPages;
-  late List<Results> results;
+import 'dart:convert';
+import 'dart:html';
+import 'package:http/http.dart' show Client, Response;
+import 'package:movie_db/model/popular_movie.dart';
 
-  PopularMovies(
-      {required this.page,
-      required this.totalResults,
-      required this.totalPages,
-      required this.results});
+class ApiProvide {
+  String apiKey= '745393cc8dcc8e8097ff9b6482981e59';
+  String baseUrl = 'https://api.themoviedb.org/3';
 
-      PopularMovies.fromJson(Map<String, dynamic> json) {
-        page = json['page'];
-        totalResults = json['total_results'];
-        totalPages = json['total_pages'];
-        if (json['results'] != null) {
-          results = <Results>[];
-          json['results'].forEach((v) {
-            results.add(new Results.fromJson(v));
-          });
-        }
-}
-Map<String, dynamic> toJson() {
-  final Map<String, dynamic> data = new Map<String, dynamic>();
-  data['page'] = this.page;
-  data['total_results'] = this.totalResults;
-  data['total_pages'] = this.totalPages;
-  if (this.results != null) {
-    data['results'] = this.results.map((v) => v.toJson()).toList();
-  }
-  return data;
-}
-}
+  Client client = Client();
 
-class Results {
-  late double popularity;
-  late int voteCount;
-  late bool video;
-  late String posterPath;
-  late int id;
-  late bool adult;
-  late String backdropPath;
-  late String originalLanguage;
-  late String originalTitle;
-  late List<int> genreIds;
-  late String title;
-  late double voteAverage;
-  late String overview;
-  late String releaseDate;
+  Future<PopularMovies> getPopularMovie() async {
+    String url = '$baseUrl/movie/popular?api_key=$apiKey';
+        Response response = await client.get(Uri.parse(url));
 
-  Results(
-    {required this.popularity,
-    required this.voteCount,
-    required this.video,
-    required this.posterPath,
-    required this.id,
-    required this.adult,
-    required this.backdropPath,
-    required this.originalLanguage,
-    required this.originalTitle,
-    required this.genreIds,
-    required this.title,
-    required this.voteAverage,
-    required this.overview,
-    required this.releaseDate});
-
-    Results.fromJson(Map<String, dynamic> json) {
-      popularity = json['popularity'];
-      voteCount = json['vote_count'];
-      video = json['video'];
-      posterPath = json['poster_path'];
-      id = json['id'];
-      adult = json['adult'];
-      backdropPath = json['backdrop_path'];
-      originalLanguage = json['original_language'];
-      originalTitle = json['original_title'];
-      genreIds = json['genre_ids'].cast<int>();
-      title = json['title'];
-      voteAverage = json['vote_average'];
-      overview = json['overview'];
-      releaseDate = json['release_date'];
+    if (response.statusCode == 200) {
+      return PopularMovies.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load popular movies');
     }
-
-    Map<String, dynamic> toJson() {
-      final Map<String, dynamic> data = new Map<String, dynamic>();
-      data ['popularity'] = this.popularity;
-      data ['vote_count'] = this.voteCount;
-      data ['video'] = this.video;
-      data ['poster_path'] = this.posterPath;
-      data ['id'] = this.id;
-      data ['adult'] = this.adult;
-      data ['backdrop_path'] = this.backdropPath;
-      data ['original_language'] = this.originalLanguage;
-      data ['original_title'] = this.originalTitle;
-      data ['genre_ids'] = this.genreIds;
-      data ['title'] = this.title;
-      data ['vote_average'] = this.voteAverage;
-      data ['overview'] = this.overview;
-      data ['release_date'] = this.releaseDate;
-      return data;
-    }
+  } 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,174 @@
+import 'dart:html';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_db/model/popular_movie.dart';
+import 'data/api_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(movieApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class movieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie DB',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  ApiProvide apiProvider = ApiProvide();
+  late Future <PopularMovies> popularMovies;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+
+@override
+void initState() {
+  popularMovies = apiProvider.getPopularMovie();
+  super.initState();
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Movie DB'),
+    ),
+    body: FutureBuilder<PopularMovies>(
+      future: popularMovies,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.results.length,
+            itemBuilder: (BuildContext context, int index) {
+              return movieItem(
+                poster:
+                '$imageBaseUrl${snapshot.data.results[index].posterPath}',
+                title: '${snapshot.data.results[index].title}',
+                date: '${snapshot.data.results[index].releaseDate}',
+                vote: '${snapshot.data.results[index].voteAverage}',
+                onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => MovieDetail(
+                    movie: snapshot.data.results[index],
+                  )));
+            },
+          );
+          });
+          } else if (snapshot.hasError) {
+          print("Has Error: ${snapshot.error}");
+          return Text('Error!!!');
+        } else {
+          print("Loading...");
+          return CircularProgressIndicator();
+        }
+        },
+    ),
+  );
+}
+
+Widget movieItem(
+    {required String poster,
+    required String title,
+    required String date,
+    required String vote,
+    required Function()? onTap}) {
+  return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        child: Card(
+          child: Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  child: CachedNetworkImage(
+                    imageUrl: poster,
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          title,
+                          style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold,
+                          ),),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.calendar_today,
+                            size: 12,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(date),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.star,
+                              size: 12,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(vote),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+class MovieDetail extends StatelessWidget {
+  final Results movie;
+  const MovieDetail({Key? key, required this.movie}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(movie.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
